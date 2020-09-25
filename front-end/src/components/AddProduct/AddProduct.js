6c60ec365483366ac560da7bbc2a27d649aa2a85
Jerry777777 <491818175@qq.com>
2020-09-25 14:42:01 +0800

    [xiawenhao] feat: add the web page detail

---
+++ front-end/src/components/AddProduct/AddProduct.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddProduct extends Component {
+  render() {
+    return (
+      <div>
+        添加货品页面暂无数据
+      </div>
+    );
+  }
+}
+
+export default AddProduct; 
---
+++ front-end/src/components/Mall/Mall.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Mall extends Component {
+  render() {
+    return (
+      <div>
+        商城页面暂无数据
+      </div>
+    );
+  }
+}
+
+export default Mall; 
