0a9a9b13073c35a3e74d8ff8970817eb80df4bad
Jerry777777 <491818175@qq.com>
2020-09-25 17:28:26 +0800

    [xiawenhao] feat: finish order page

--- front-end/src/components/Order/Order.js
+++ front-end/src/components/Order/Order.js
@@ -1,10 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {Table, Button} from 'antd';
+import './Order.css';
 
 class Order extends Component {
+  state = {
+    data: [],
+    products: [],
+    orders: []
+  }
+
+  getOrders() {
+    fetch("/orders", {method: 'GET'})
+      .then(res => res.json())
+      .then(data => {
+        this.setState({orders: data})
+        return fetch("/products", { method: 'GET'})
+      })
+      .then(res => res.json())
+      .then(data => {this.setState({products: data})})
+      .then(() => this.setState({data: this.state.orders.map(item => {
+          let currentProduct = this.state.products.find(product => product.id === item.productId);
+          return {
+            name: currentProduct.name,
+            price: currentProduct.price,
+            count: item.count,
+            unit: currentProduct.unit,
+          }
+        })}))
+      .catch(error => console.error(error))
+  }
+
+  componentDidMount() {
+    this.getOrders();
+  }
+
   render() {
+    const columns = [
+      {
+        title: '名字',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '单价',
+        dataIndex: 'price',
+        key: 'price',
+      },
+      {
+        title: '数量',
+        dataIndex: 'count',
+        key: 'count',
+      },
+      {
+        title: '单位',
+        key: 'unit',
+        dataIndex: 'unit',
+      },
+      {
+        title: '操作',
+        key: 'action',
+        render: () => <Button danger>删 除</Button>
+      },
+    ];
+
+    const data = this.state.data;
+
     return (
-      <div>
-        订单页面暂无数据
+      <div className="order-list">
+        {data.length === 0 ?
+          <span>暂无订单，返回<a href="/">商城页面</a>继续购买</span> :
+          <Table columns={columns} dataSource={data} pagination={false}/>}
       </div>
     );
   }
