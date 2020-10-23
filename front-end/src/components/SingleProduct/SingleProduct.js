8aff2ee1b75a75992516cb324c8c77a74279df43
Jerry777777 <491818175@qq.com>
2020-10-23 11:30:30 +0800

    [xiawenhao] feat: change the good price inti english

--- front-end/src/components/SingleProduct/SingleProduct.js
+++ front-end/src/components/SingleProduct/SingleProduct.js
@@ -7,7 +7,7 @@ class SingleProduct extends Component {
       <div className="single-product">
         <img src={this.props.imageUrl} alt={this.props.name}/>
         <h4>{this.props.name}</h4>
-        <p>单价：{this.props.price}元/{this.props.unit}</p>
+        <p>price：{this.props.price}rmb/{this.props.unit}</p>
         <button
             disabled={this.props.isDisabled}
             onClick={() => this.props.onAddProduct(this.props.id)}>十</button>
